c10f9cd209554c7575536cdc3924b5b151b92805
satyaupendra <satyaupendra.devabhaktineni9@gmail.com>
2022-05-22 12:28:19 -0400

    added cart dropdown

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
 
 import "./index.scss";
+import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,7 +14,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
@@ -23,4 +26,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
